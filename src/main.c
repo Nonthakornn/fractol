fb419c1e2cdabb7626d81d86d2d18ebd2893c3b1
Nonthakorn <nonthakorn.che@gmail.com>
2025-01-21 23:33:08 +0700

    update main

--- src/main.c
+++ src/main.c
@@ -6,56 +6,21 @@
 /*   By: nchencha <nchencha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 02:50:42 by nchencha          #+#    #+#             */
-/*   Updated: 2025/01/16 13:03:31 by nchencha         ###   ########.fr       */
+/*   Updated: 2025/01/21 22:47:53 by nchencha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-void my_key(mlx_key_data_t keydata, void *param)
+int main(int argc, char **argv)
 {
-	if (keydata.key == MLX_KEY_J && keydata.action == MLX_PRESS)
-		puts("Hello ");
-	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
-	{
-		puts("Exit");
-		mlx_close_window(param);
-	}
-}
+	t_data data;
 
-int    main(int ac, char **av) 
-{
-    (void)ac;
-    (void)av;
-
-    mlx_t *mlx;
-	mlx_image_t *img;
-
-	mlx_set_setting(MLX_DECORATED, 1);
-    mlx = mlx_init(WIDTH, HEIGHT, "fractol", true);
-
-	//Create image buffer where you can draw pixels
-	img = mlx_new_image(mlx, 256, 256);
-
-	//Draw red pixel at coordinates
-	  for (int y = 0; y < 256; y++)
-    {
-        for (int x = 0; x < 256; x++)
-        {
-			//Display image on that window position
-            mlx_put_pixel(img, x, y, RED);
-        }
-    }
-	//Placing a picture on window at specific coordinate
-	mlx_image_to_window(mlx, img, 0 , 0);
-	mlx_image_to_window(mlx, img, 300 , 0);
-	mlx_image_to_window(mlx, img,  0, 300);
-
-	
-	mlx_key_hook(mlx, (mlx_keyfunc)(my_key), mlx);
-	//mlx_key_hook(mlx, &my_key, mlx);
-
-	mlx_loop(mlx);
-    mlx_terminate(mlx);
-	return (0);
+	if (!valid_args(argc, argv))
+		err_msg(ERR_ARGS);
+	init_fractol(&data, argc);
+	draw_image(&data);
+	event_listener(&data);
+	mlx_loop(data.mlx);
+	mlx_terminate(data.mlx);
 }
